5afc64297e872d2b716a90ca79394496453ac1b2
codycoolwaffle <codyhaslam@gmail.com>
2016-03-08 10:19:00 -0800

    Finished a12

--- src/ccw/a12/a12.java
+++ src/ccw/a12/a12.java
@@ -5,45 +5,79 @@ import java.util.ArrayList;
 
 public class a12
 {
-    public void findAverage()
-    {
+	public static double findAverage(ArrayList<Integer> scores)
+	{
+		double average = 0;
+		for(int i : scores)
+			average += i;
+		return average/scores.size();
+	}
 
-    }
+	public static double findMedian(ArrayList<Integer> scores)
+	{
+		double median;
+		if(scores.size()%2==1)
+			median = scores.get((scores.size()/2));
+		else
+			median = (scores.get(scores.size()/2) + scores.get((scores.size()/2)-1))/2;
+		return median;
+	}
 
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
+	public static int findRange(ArrayList<Integer> scores)
+	{
+		return scores.get(scores.size()-1)-scores.get(0);
+	}
 
-        ArrayList<Integer> scores = new ArrayList<Integer>();
-        int in;
-        while (true)
-        {
-            in = scan.nextInt();
-            if (in==-1)
-                break;
-            scores.add(in);
-        }
-        System.out.println(scores);
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
 
-        for(int i = 0; i < scores.size()-1; i++)
-        {
-            int lowNum = scores.get(i);
-            int lowNumPos = i;
-            for(int i2 = i; i2 < scores.size(); i2++)
-            {
-                if(lowNum > scores.get(i2))
-                {
-                    lowNum = scores.get(i2);
-                    lowNumPos = i2;
-                }
-            }
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		System.out.println("Input:");
+		int in = scan.nextInt();
+		while(in != -1)
+		{
+			if(in>=0 && in<=100)
+				scores.add(in);
+			in = scan.nextInt();
+		}
+		System.out.println("\nOutput:\n");
+		System.out.println(scores);
 
-            int temp = scores.get(i);
-            scores.set(i, scores.get(lowNumPos));
-            scores.set(lowNumPos, temp);
-        }
-        System.out.println(scores);
+		for(int i = 0; i < scores.size() - 1; i++)
+		{
+			int lowNum = scores.get(i);
+			int lowNumPos = i;
+			for(int i2 = i; i2 < scores.size(); i2++)
+			{
+				if(lowNum > scores.get(i2))
+				{
+					lowNum = scores.get(i2);
+					lowNumPos = i2;
+				}
+			}
 
+			int temp = scores.get(i);
+			scores.set(i, scores.get(lowNumPos));
+			scores.set(lowNumPos, temp);
+		}
+		System.out.println("Sorted:\n" + scores);
 
-    }
+		System.out.println("Average: " + findAverage(scores));
+
+		System.out.println("Median: " + findMedian(scores));
+
+		System.out.println("Range: " + findRange(scores));
+
+		scores.remove(scores.size()-1);
+		scores.remove(0);
+
+		System.out.println("Minimum and maximum values removed:\n" + scores);
+
+		System.out.println("Average: " + findAverage(scores));
+
+		System.out.println("Median: " + findMedian(scores));
+
+		System.out.println("Range: " + findRange(scores));
+	}
 }
